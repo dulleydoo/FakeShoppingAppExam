a6aa1d211de4b5ac7b34f0a413c9781d2605f15f
dulleydoo <karem.jabar2015@gmail.com>
2022-09-27 15:41:22 -0400

    Done

--- src/main/java/ShopApp.java
+++ src/main/java/ShopApp.java
@@ -3,8 +3,10 @@ import java.util.ArrayList;
 public class ShopApp {
     public static double calcTotal(Clothing[] clothes) {
         int i;
+        double total = 0.0;
         for (i = 0; i < clothes.length-1; i++) {
-        }return (clothes[i].getPrice());
+            total += clothes[i].getPrice();
+        }return total;
     }
     public static boolean isAFit(Customer customer, Clothing clothe){
         if (customer.getSize() == clothe.getSize()) {
